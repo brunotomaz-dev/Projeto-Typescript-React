b20aaeffd848439a7e61685936cb3c5b0d587274
brunotomaz-dev <tomaz.bruno@gmail.com>
2025-07-14 09:45:23 -0300

    ✨ feat: Adicionar página de preenchimento de operadores e atualizar permissões de acesso

---
+++ src/pages/Operators/OperatorsFilling.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OperationFillingPage: React.FC = () => {
+  /* --------------------------------------------------------------------------------------------------------- */
+  /*                                                   LAYOUT                                                  */
+  /* --------------------------------------------------------------------------------------------------------- */
+  return (
+    <div>
+      <h1>Operadores</h1>
+      <p>Esta página está em construção.</p>
+      <p>Em breve, você poderá visualizar e gerenciar os operadores aqui.</p>
+    </div>
+  );
+};
+
+export default OperationFillingPage;
